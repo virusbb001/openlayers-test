6df704483e731d5f08f9ae699c936ee795b58e87
virus <virusbb001a@gmail.com>
2016-06-01 08:15:50 +0900

    add babel task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp=require("gulp"),
+    babel=require("gulp-babel"),
+    sourcemaps=require("gulp-sourcemaps"),
+    plumber=require("gulp-plumber"),
     browserSync = require("browser-sync").create();
+
+gulp.task("babel",function(){
+ return gulp
+  .src("src/**/*.js")
+  .pipe(plumber())
+  .pipe(babel())
+  .dest("js")
+});
 
 gulp.task("server",function(){
  browserSync.init({
