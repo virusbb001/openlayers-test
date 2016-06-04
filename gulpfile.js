85a5aa2556aae6a82ffb23af7bd4b5215feba028
virus <virusbb001a@gmail.com>
2016-06-05 00:22:58 +0900

    add eslint task and concat task

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,44 @@
 var gulp=require("gulp"),
     babel=require("gulp-babel"),
+    concat=require("gulp-concat"),
     sourcemaps=require("gulp-sourcemaps"),
     plumber=require("gulp-plumber"),
+    eslint=require("gulp-eslint"),
     browserSync = require("browser-sync").create();
 
+gulp.task("eslint",function(){
+  return gulp
+    .src("src/**/*.js")
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
 gulp.task("babel",function(){
- return gulp
-  .src("src/**/*.js")
-  .pipe(plumber())
-  .pipe(babel())
-  .dest("js")
+  return gulp
+    .src("src/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(concat("index.js"))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("js"));
 });
 
 gulp.task("server",function(){
- browserSync.init({
-  server:{
-   baseDir: "./"
-  }
- });
+  browserSync.init({
+    server:{
+      baseDir: "./"
+    }
+  });
 });
 
-gulp.task("js-watch",["babel"],browserSync.reload);
+gulp.task("js-watch",["eslint","babel"],browserSync.reload);
 
 gulp.task("watch",["server"],function(){
- gulp.watch("./index.*",browserSync.reload);
- gulp.watch("src/**/*.js",["js-watch"]);
+  gulp.watch("./index.*",browserSync.reload);
+  gulp.watch("src/**/*.js",["js-watch"]);
 });
 
-gulp.task("default",["watch"]);
+gulp.task("default",["watch","js-watch"]);
