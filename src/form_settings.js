069e885e57ef47189075b073795a9b78be1da75d
virus <virusbb001a@gmail.com>
2016-06-05 00:58:56 +0900

    add settings

---
+++ src/form_settings.js
@@ -0,0 +1,23 @@
+/**
+ * パラメータ入力フォームと実行の設定を返す関数
+ * @param ol OpenLayer3 のオブジェクト
+ * @param map 表示用のMapオブジェクト
+ */
+function getOptions(ol,map){
+  var options = {
+    "add_single_ballon": {
+      head: "Add Single Balloon",
+      inputs: ["latitude","longitude"],
+      execute: function(datas){
+        var latitude=datas.latitude;
+        var longitude=datas.longitude;
+        window.alert(latitude);
+        window.alert(longitude);
+      }
+    }
+  };
+
+  return options;
+}
+
+export default getOptions;
