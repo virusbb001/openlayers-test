436774f0ff0154c427d9637d9a550552c8527239
virus <virusbb001a@gmail.com>
2016-06-04 23:53:13 +0900

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+ "globals": {
+  "ol": true
+ },
+ "env": {
+  "browser": true,
+  "es6": true
+ },
+ "extends": "eslint:recommended",
+ "installedESLint": true,
+ "parserOptions": {
+  "ecmaFeatures": {
+   "experimentalObjectRestSpread": true,
+   "jsx": true
+  },
+  "sourceType": "module"
+ },
+ "plugins": [
+  "react"
+ ],
+ "rules": {
+  "indent": [
+   "error",
+   2,
+   {
+    "VariableDeclarator": {
+     "var": 2,
+     "let": 2,
+     "const": 3
+    }
+   }
+  ],
+  "linebreak-style": [
+   "error",
+   "unix"
+  ],
+  "quotes": [
+   "error",
+   "double"
+  ],
+  "semi": [
+   "error",
+   "always"
+  ]
+ }
+};
