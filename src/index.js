2224e0c2e21730af7fa070247cc4c689fc976a0d
virus <virusbb001a@gmail.com>
2016-06-05 23:50:57 +0900

    change source to OSM

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ document.addEventListener("DOMContentLoaded",function(){
   var map=new ol.Map({
     target: "map",
     layers: [
-      new ol.layer.Tile({
-        source: new ol.source.MapQuest({layer: "sat"})
-      })
+      new ol.layer.Tile({ source: new ol.source.OSM() })
     ],
     view: new ol.View({
       center: ol.proj.fromLonLat([37.41, 8.82]),
