9fdd977c587b01b866fb5447c765cfc985d79fb0
virus <virusbb001a@gmail.com>
2016-06-06 00:45:52 +0900

    add point and add js/*.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,33 @@ import getOpts from "form_settings";
 import render from "dom_components";
 
 document.addEventListener("DOMContentLoaded",function(){
+  var meganeMuseum= new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([136.1988424,35.9427557]))
+  });
+  meganeMuseum.setStyle(new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 7,
+      snapToPixel: false,
+      fill: new ol.style.Fill({color: "black"}),
+      stroke: new ol.style.Stroke({
+        color: "white", width: 2
+      })
+    })
+  }));
+
+  var vectorSource = new ol.source.Vector({
+    features: [meganeMuseum]
+  });
+
   var map=new ol.Map({
     target: "map",
     layers: [
-      new ol.layer.Tile({ source: new ol.source.OSM() })
+      new ol.layer.Tile({ source: new ol.source.OSM() }),
+      new ol.layer.Vector({ source: vectorSource })
     ],
     view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4
+      center: ol.proj.fromLonLat([136.223333,36.062083]),
+      zoom: 14
     })
   });
   var options=getOpts(ol,map);
