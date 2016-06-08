a58a44386ee6302d714013c03e2bd7bda30bc456
virus <virusbb001a@gmail.com>
2016-06-09 00:11:41 +0900

    add for lat-lng form and set to now position

--- src/dom_settings.js
+++ src/dom_settings.js
@@ -1,4 +1,30 @@
 function create_components(){
+  var latlngInputs=new Vue({
+    el: "#forms",
+    data: {
+      position: {
+        latitude: 0,
+        longitude: 0
+      }
+    },
+    methods: {
+      setFromNowPlace: function(event){
+        event.target.disabled=true;
+        event.target.textContent="Loading..."
+        navigator.geolocation.getCurrentPosition((position)=>{
+          this.$data.position.latitude=position.coords.latitude;
+          this.$data.position.longitude=position.coords.longitude;
+          event.target.textContent="Set now Position"
+          event.target.disabled=false;
+        });
+      },
+      checkGeolocation: function(){
+        return !("geolocation" in navigator);
+      }
+    }
+  });
+
+  window.latlngInputs=latlngInputs;
 }
 
 export default create_components;
